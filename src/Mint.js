b661e41020216f382a7c276dd64286388d4d5ab6
hatem <hatemazaiez1@gmail.com>
2022-08-26 13:17:36 +0100

    new

---
+++ src/Mint.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+
+function Mint(props) {
+
+  return (
+    <div id="content-container" >
+    <div id="_bg__frame_1"  ></div>
+    <div id="__img___rectangle_and_bgimg"  ></div>
+    <div id="__img___design_left_circle"  ></div>
+    <div id="exclusive_mint" >
+        EXCLUSIVE MINT
+    </div>
+    <div id="days" >
+        DAYS
+    </div>
+    <div id="hours" >
+        HOURS
+    </div>
+    <div id="mins" >
+        MINS
+    </div>
+    <div id="sec" >
+        SEC
+    </div>
+    <div id="_7" >
+        7
+    </div>
+    <div id="_14" >
+        14
+    </div>
+    <div id="_27" >
+        27
+    </div>
+    <div id="_58" >
+        58
+    </div>
+    <div id="__img___group_1"  ></div>
+    <div id="_120___7777" >
+        120 / 7777
+    </div>
+    <div id="nft_collectible_project_of_unique__digitally_hand_drown_avatars_immortalized_on_the_ethereum_blockchain" >
+        NFT collectible project of unique, digitally hand-drown avatars immortalized on the Ethereum blockchain
+    </div>
+    <div id="_7777_generative_avatar_characters_with_over_100_hand_frawn_features" >
+        7777 Generative avatar characters with over 100 hand-frawn features
+    </div>
+    <div id="vector"  ></div>
+    <div id="vector_ek1"  ></div>
+    <div id="vector_ek2"  ></div>
+    <div id="__img___group_2"  ></div>
+    <div id="__img___group_1_ek1"  ></div>
+    <div id="__img___group_3"  ></div>
+
+    <div id="frame_2_1"  >
+        <div id="vector_ek3"  ></div>
+        <div id="vector_ek4"  ></div>
+        <div id="vector_ek5"  ></div>
+        <div id="vector_ek6"  ></div>
+
+    </div>
+    <div id="mint" >
+        Mint
+    </div>
+
+    <div id="frame_3_2"  >
+        <div id="vector_ek7"  ></div>
+        <div id="vector_ek8"  ></div>
+        <div id="vector_ek9"  ></div>
+        <div id="vector_ek10"  ></div>
+
+    </div>
+    <div id="connect_wallet" >
+        Connect Wallet
+    </div>
+
+</div>
+  );
+}
+
+export default withRouter(Mint);
